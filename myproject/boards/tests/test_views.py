2e0f3e4f588d9360fe2a981daa085c7fc20a63f0
Samy MAURICE-PEROUMAL <c00243742@itcarlow.ie>
2019-01-25 11:48:57 +0000

    End of part 4. User can login, logout, sing up, reset and change password.

--- myproject/boards/tests/test_views.py
+++ myproject/boards/tests/test_views.py
@@ -2,9 +2,9 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
 from django.urls import resolve
-from .views import home, board_topics, new_topic
-from .models import Board, Topic, Post
-from .forms import NewTopicForm
+from ..views import home, board_topics, new_topic
+from ..models import Board, Topic, Post
+from ..forms import NewTopicForm
 
 class HomeTests(TestCase):
     def setUp(self):
