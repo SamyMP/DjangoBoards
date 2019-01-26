7611069f6c17e19b4eee97e050476098e487f9cb
Samy MAURICE-PEROUMAL <c00243742@itcarlow.ie>
2019-01-26 16:23:35 +0000

    Improved pagination, added Markdown in Pipfile, ran black for the first time on the entire project with 'black .'.

--- myproject/boards/models.py
+++ myproject/boards/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import Truncator
+from markdown import markdown
+from django.utils.html import mark_safe
+
 
 class Board(models.Model):
     name = models.CharField(max_length=30, unique=True)
@@ -8,30 +11,37 @@ class Board(models.Model):
 
     def __str__(self):
         return self.name
-    
+
     def get_posts_count(self):
         return Post.objects.filter(topic__board=self).count()
 
     def get_last_post(self):
-        return Post.objects.filter(topic__board=self).order_by('-created_at').first()
+        return Post.objects.filter(topic__board=self).order_by("-created_at").first()
+
 
 class Topic(models.Model):
     subject = models.CharField(max_length=255)
     last_updated = models.DateTimeField(auto_now_add=True)
-    board = models.ForeignKey(Board, on_delete = models.CASCADE, related_name='topics')
-    starter = models.ForeignKey(User, on_delete = models.CASCADE, related_name='topics')
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name="topics")
+    starter = models.ForeignKey(User, on_delete=models.CASCADE, related_name="topics")
     views = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.subject
 
+
 class Post(models.Model):
     message = models.TextField(max_length=4000)
-    topic = models.ForeignKey(Topic, on_delete = models.CASCADE, related_name='posts')
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name="posts")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(null=True)
-    created_by = models.ForeignKey(User, on_delete = models.CASCADE, related_name='posts')
-    updated_by = models.ForeignKey(User, on_delete = models.CASCADE, null=True, related_name = '+')
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    updated_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, related_name="+"
+    )
+
+    def get_message_as_markdown(self):
+        return mark_safe(markdown(self.message, safe_mode='escape'))
 
     def __str__(self):
         truncated_message = Truncator(self.message)
--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -14,13 +14,16 @@ from .forms import NewTopicForm, PostForm
 
 class BoardListView(ListView):
     model = Board
-    context_object_name = 'boards'
-    template_name = 'home.html'
+    context_object_name = "boards"
+    template_name = "home.html"
+
 
 def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
-    queryset = board.topics.order_by('-last_updated').annotate(replies=Count('posts') - 1)
-    page = request.GET.get('page', 1)
+    queryset = board.topics.order_by("-last_updated").annotate(
+        replies=Count("posts") - 1
+    )
+    page = request.GET.get("page", 1)
 
     paginator = Paginator(queryset, 20)
 
@@ -34,13 +37,13 @@ def board_topics(request, pk):
         # in the url, so we fallback to the last page
         topics = paginator.page(paginator.num_pages)
 
-    return render(request, 'topics.html', {'board': board, 'topics': topics})
+    return render(request, "topics.html", {"board": board, "topics": topics})
 
 
 @login_required
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
-    if request.method == 'POST':
+    if request.method == "POST":
         form = NewTopicForm(request.POST)
         if form.is_valid():
             topic = form.save(commit=False)
@@ -48,59 +51,64 @@ def new_topic(request, pk):
             topic.starter = request.user  # <- here
             topic.save()
             Post.objects.create(
-                message=form.cleaned_data.get('message'),
+                message=form.cleaned_data.get("message"),
                 topic=topic,
-                created_by=request.user  # <- and here
+                created_by=request.user,  # <- and here
             )
-            return redirect('topic_posts', pk=pk, topic_pk=topic.pk)
+            return redirect("topic_posts", pk=pk, topic_pk=topic.pk)
     else:
         form = NewTopicForm()
-    return render(request, 'new_topic.html', {'board': board, 'form': form})
+    return render(request, "new_topic.html", {"board": board, "form": form})
+
 
 def topic_posts(request, pk, topic_pk):
     topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
     topic.views += 1
     topic.save()
-    return render(request, 'topic_posts.html', {'topic': topic})
+    return render(request, "topic_posts.html", {"topic": topic})
+
 
 @login_required
 def reply_topic(request, pk, topic_pk):
     topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
-    if request.method == 'POST':
+    if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.topic = topic
             post.created_by = request.user
             post.save()
-            return redirect('topic_posts', pk=pk, topic_pk=topic_pk)
+            return redirect("topic_posts", pk=pk, topic_pk=topic_pk)
     else:
         form = PostForm()
-    return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
+    return render(request, "reply_topic.html", {"topic": topic, "form": form})
+
 
 def new_post(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('post_list')
+            return redirect("post_list")
     else:
         form = PostForm()
-    return render(request, 'new_post.html', {'form': form})
+    return render(request, "new_post.html", {"form": form})
+
 
 class NewPostView(CreateView):
     model = Post
     form_class = PostForm
-    success_url = reverse_lazy('post_list')
-    template_name = 'new_post.html'
+    success_url = reverse_lazy("post_list")
+    template_name = "new_post.html"
 
-@method_decorator(login_required, name='dispatch')
+
+@method_decorator(login_required, name="dispatch")
 class PostUpdateView(UpdateView):
     model = Post
-    fields = ('message', )
-    template_name = 'edit_post.html'
-    pk_url_kwarg = 'post_pk'
-    context_object_name = 'post'
+    fields = ("message",)
+    template_name = "edit_post.html"
+    pk_url_kwarg = "post_pk"
+    context_object_name = "post"
 
     def get_queryset(self):
         queryset = super().get_queryset()
@@ -111,4 +119,42 @@ class PostUpdateView(UpdateView):
         post.updated_by = self.request.user
         post.updated_at = timezone.now()
         post.save()
-        return redirect('topic_posts', pk=post.topic.board.pk, topic_pk=post.topic.pk)
+        return redirect("topic_posts", pk=post.topic.board.pk, topic_pk=post.topic.pk)
+
+
+class TopicListView(ListView):
+    model = Topic
+    context_object_name = "topics"
+    template_name = "topics.html"
+    paginate_by = 20
+
+    def get_context_data(self, **kwargs):
+        kwargs["board"] = self.board
+        return super().get_context_data(**kwargs)
+
+    def get_queryset(self):
+        self.board = get_object_or_404(Board, pk=self.kwargs.get("pk"))
+        queryset = self.board.topics.order_by("-last_updated").annotate(
+            replies=Count("posts") - 1
+        )
+        return queryset
+
+
+class PostListView(ListView):
+    model = Post
+    context_object_name = "posts"
+    template_name = "topic_posts.html"
+    paginate_by = 2
+
+    def get_context_data(self, **kwargs):
+        self.topic.views += 1
+        self.topic.save()
+        kwargs["topic"] = self.topic
+        return super().get_context_data(**kwargs)
+
+    def get_queryset(self):
+        self.topic = get_object_or_404(
+            Topic, board__pk=self.kwargs.get("pk"), pk=self.kwargs.get("topic_pk")
+        )
+        queryset = self.topic.posts.order_by("created_at")
+        return queryset
